60b38080accba32806505ecd857371a8bb6b7bd4
danilorlimma <danilo.rodrigues.lima@hotmail.com>
2024-07-29 23:00:30 -0300

    Implementando Login

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl:'https://notes-serverless-app.com',
-    defaultCommandTimeout:5000,
+    defaultCommandTimeout:50000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
