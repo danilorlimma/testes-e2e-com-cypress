b90a912ca1e85ed3d6f26b55db86498d5ff83583
danilorlimma <danilo.rodrigues.lima@hotmail.com>
2024-08-06 23:19:35 -0300

    Run all specs

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl:'https://notes-serverless-app.com',
+    experimentalRunAllSpecs:true,
     defaultCommandTimeout:9000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
