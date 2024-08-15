d921918b8a5155e485085daa4486f61d09ae2542
danilorlimma <danilo.rodrigues.lima@hotmail.com>
2024-08-14 23:23:12 -0300

    Implementa teste responsivo

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    chromeWebSecurity:false
+    chromeWebSecurity:false,
+    env:{viewportWidthBreakpoint:768,}
   },
 })
--- cypress/e2e/authenticated.cy.js
+++ cypress/e2e/authenticated.cy.js
@@ -42,6 +42,11 @@ describe('Scenarios where authentication is a pre-condition', () => {
   it.only('logs out', () => {
     cy.visit('/')
     cy.wait('@getNotes')
+    if (Cypress.config('viewportWidth')<Cypress.env('viewportWidthBreakpoint')) {
+      cy.get('.navbar-toggle.collapsed')
+        .should('be.visible')
+        .click()
+    }
     cy.contains('.nav a','Logout').click()
     cy.get('#email').should('be.visible')
   })
