a6ff17ae6e349ed2948ceac833c45c917002ea4f
danilorlimma <danilo.rodrigues.lima@hotmail.com>
2025-08-04 10:42:15 -0300

    Teste CI

--- cypress/e2e/authenticated.cy.js
+++ cypress/e2e/authenticated.cy.js
@@ -2,7 +2,7 @@
 
 import { faker } from '@faker-js/faker/locale/en'
 
-describe('Scenarios where authentication is a pre-condition', () => {
+describe('Scenarios where a authentication is a pre-condition', () => {
   beforeEach(()=>{
     cy.intercept('GET','**/notes').as('getNotes')
     cy.sessionLogin()
